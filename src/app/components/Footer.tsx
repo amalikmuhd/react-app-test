f83e92dfaa02dfcb3c815e3d2b0de265e274293a
Abdulmalik Muhammad <bawasuru@gmail.com>
2023-05-16 17:06:47 +0100

    feat: add static testing

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,17 +4,17 @@ import forbes from "../assets/Forbes.svg";
 import entreprenuer from "../assets/Entrepreneur.svg";
 import techTimes from "../assets/TechCrunch.png";
 import wallStreets from "../assets/TheWallStreet.svg";
-import { FooterContainer, FooterInnerContainer } from "./Footer.styled";
+import {FooterContainer, FooterInnerContainer} from "./Footer.styled";
 
 const Footer = () => {
   return (
     <FooterContainer>
       <FooterInnerContainer>
-        <Image src={techCrunch} alt="phones" style={{ width: "150px" }} />
-        <Image src={forbes} alt="phones" style={{ width: "150px" }} />
-        <Image src={entreprenuer} alt="phones" style={{ width: "150px" }} />
-        <Image src={techTimes} alt="phones" style={{ width: "150px" }} />
-        <Image src={wallStreets} alt="phones" style={{ width: "150px" }} />
+        <Image src={techCrunch} alt="phones" style={{width: "150px"}} />
+        <Image src={forbes} alt="phones" style={{width: "150px"}} />
+        <Image src={entreprenuer} alt="phones" style={{width: "150px"}} />
+        <Image src={techTimes} alt="phones" style={{width: "150px"}} />
+        <Image src={wallStreets} alt="phones" style={{width: "150px"}} />
       </FooterInnerContainer>
     </FooterContainer>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
-import { Inter, Rubik } from "next/font/google";
+import {Inter} from "next/font/google";
 // SF Pro Text
-const inter = Inter({ subsets: ["latin"] });
-const rubik = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
+const rubik = Inter({subsets: ["latin"]});
 
 // import localFont from "@next/font/local";
 
@@ -25,11 +25,7 @@ export const metadata = {
   description: "Zero Fees, No Credit Check",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={(inter.className, rubik.className)}>{children}</body>
